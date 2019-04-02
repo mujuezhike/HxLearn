c2040419ecfb462b2496560fc2f809df292478f7
mujuezhike <37540006@qq.com>
2019-04-02 09:36:29 +0800

    'start'

---
+++ HxLearn/BlackCat.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HxLearn
+{
+    class BlackCat:Cat
+    {
+
+        public BlackCat(int m )
+        {
+    
+        }
+
+        public BlackCat()
+        {
+
+        }
+    }
+}
