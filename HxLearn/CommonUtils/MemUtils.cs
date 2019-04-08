a2b038d2ecf610f02b4d96705cb14ba51bef1118
mujuezhike <37540006@qq.com>
2019-04-08 13:46:48 +0800

    'memutils'

---
+++ HxLearn/CommonUtils/MemUtils.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HxLearn.CommonUtils
+{
+    public static class MemUtils
+    {
+        public static string GetMemoryAddress(Object o)
+        {
+            GCHandle h = GCHandle.Alloc(o, GCHandleType.WeakTrackResurrection);
+            IntPtr addr = GCHandle.ToIntPtr(h);
+            return "0x" + addr.ToString("X");
+        }
+    }
+}
--- HxLearn/Program.cs
+++ HxLearn/Program.cs
@@ -1,10 +1,7 @@
-﻿using HxLearn.GameObject;
-using HxLearn.GameStatic;
+﻿using HxLearn.GameStatic;
 using System;
 using System.Drawing;
-using System.Net.Http;
 using System.Threading;
-using static HxLearn.ImageLoader;
 
 namespace HxLearn
 {
@@ -12,7 +9,6 @@ namespace HxLearn
     {
         static void Main(string[] args)
         {
-            int[][] a =new  int[2][3];
 
             ViewStatic.InitConsole();
             ViewStatic.InitLayer(ref ViewStatic.BackLayer);
@@ -41,62 +37,7 @@ namespace HxLearn
             }
 
             Console.ReadLine();
-            
-            /*
-            Monster er = new Monster();
-            int i = er[1];
-            er.Name = "ername";
-            EliteMonster em = 32;
-            em.Name = "sds";
-            Monster emm = em;
-            em = (EliteMonster)emm;
-
-            string ename = (er as EliteMonster).Name;
-            Console.WriteLine(ename);
-
-                var client = new HttpClient();
-            var uri = new Uri(Uri.EscapeUriString("http://www.baidu.com"));
-            var str = client.GetAsync(uri).Result.Content.ReadAsStringAsync().Result.ToString();
-
-            GetName("http://www.baidu.com");
-            Console.WriteLine("cc:1");
-
-
-            string s = "{'id':14,'name':'dsss'}";
-
-            Monster rb = JsonConvert.DeserializeObject<Monster>(s);
-            Console.WriteLine(rb.Id);
-            Console.WriteLine(rb.Name);
-
-            string m = JsonConvert.SerializeObject(rb);
-            Console.WriteLine(m);
-
-            string ls = "[{'id':14,'name':'dsss'},{'id':15,'name':'dcss'},{'id':16,'name':'90ss'}]";
-            List<Monster> lm = JsonConvert.DeserializeObject<List<Monster>>(ls);
-
-            Console.WriteLine(lm[0].Id);
-            Console.WriteLine(lm[1].Id);
-            Console.WriteLine(lm[2].Name);
-
-            string ml = JsonConvert.SerializeObject(lm);
-            Console.WriteLine(ml);
-            
-            DateTime dt = DateTime.Now;
-           
-            Console.WriteLine(string.Format("{0:D}", dt));
-            Console.WriteLine(string.Format("{0:d}", dt));
-            Console.WriteLine(string.Format("{0:yy-MM-dd hh:mm:ss}", dt));
-            Console.ReadLine();
-            */
         }
 
-        
-        async static void GetName(string uri)
-        {
-            var client = new HttpClient();
-            var task = await client.GetAsync(uri);
-            Thread.Sleep(1000);
-            Console.WriteLine( "cc:2");
-        }
     }
 }
